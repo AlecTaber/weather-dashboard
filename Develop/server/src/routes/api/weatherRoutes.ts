6dd36602f390605e5043597d68456e2ca9785927
Alec Taber <alectaber12@gmail.com>
2024-08-09 19:49:16 -0700

    fix errors in weatherRoutes.ts

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+ import HistoryService from '../../service/historyService.js';
+ import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
   try {
     const cityName = req.body.cityName;
-    const weatherData = await WeatherService.getWeatherData(cityName);
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
 
-    await HistoryService.saveCity(cityName);
+    await HistoryService.addCity(cityName);
     res.json(weatherData);
   } catch (err) {
     console.log(err);
@@ -21,9 +21,9 @@ router.post('/', (req, res) => {
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {
+router.get('/history', async (_, res) => {
   try {
-    const searchHistory = await HistoryService.getSearchHistory();
+    const searchHistory = await HistoryService.getCities();
     res.json(searchHistory);
   } catch (err) {
     console.log(err);
@@ -35,7 +35,7 @@ router.get('/history', async (req, res) => {
 router.delete('/history/:id', async (req, res) => {
   try {
     const id = req.params.id;
-    const deletionResult = await HistoryService.deleteCityById(id);
+    const deletionResult = await HistoryService.removeCity(id);
     res.json(deletionResult);
   } catch (err) {
     console.log(err);
